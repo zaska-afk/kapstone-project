28833fe293069ea79d788a35133d80e7351b992b
Anna Amick <45767815+aamick@users.noreply.github.com>
2021-04-09 09:34:18 -0400

    added another view

--- src/views/SurveyView.js
+++ src/views/SurveyView.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { Card, CardDeck } from "react-bootstrap";
+import { Card, CardDeck, Image } from "react-bootstrap";
 
 import NavBar from "../components/NavBar";
+import PopcornContainers from "../assets/PopcornContainers.jpg";
 
 function SurveyView() {
   return (
     <>
       <NavBar />
-
+      <Image
+        src={PopcornContainers}
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1",
+        }}
+      />
       <h1 className="home-header">Movies Survey</h1>
       <h2 className="text">To Get Started, Select Ten Movies You Enjoy</h2>
       <CardDeck>
