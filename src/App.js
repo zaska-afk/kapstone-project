47d5723389d96acc73654aca584be69e7c25359f
zaska-afk <zacharybalaska@gmail.com>
2021-04-09 22:20:08 +0800

    package-lock.json merge fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodo] = useState([]);
+
+  function testGet() {
+    fetch("http://localhost:3000")
+      .then((res) => res.text())
+      .then((data) => console.log(data));
+  }
+  function testGetData() {
+    fetch("http://localhost:3000/todos")
+      .then((res) => res.json())
+      .then((data) => setTodo(data));
+  }
+
+  function testPostTodos() {
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: "testing frontend post", completed: true }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={testGet}>Test get request</button>
+      <button onClick={testGetData}>Test get todos request</button>
+      <button onClick={testPostTodos}>Test get request</button>
+      {todos.map((todo, index) => {
+        return (
+          <div key={index}>
+            <div>Title: {todo.title}</div>
+            <div>Completed: {todo.completed.toString()}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
