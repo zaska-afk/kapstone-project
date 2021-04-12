74e38ce6cd8bcaac19da8a835c125a2f483adb0b
KeishaPierre <hopeamyturner@gmail.com>
2021-04-12 10:34:12 -0400

    trying to pull

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Profile from "../components/Profile";
 import NavBar from "../components/NavBar";
 
 function ProfileView() {
   return (
     <>
       <NavBar />
+      <Profile/>
     </>
   );
 }
